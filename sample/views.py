9679a388e6c3bd9a94d09ed43400cd5391fda4f1
Dipesh Dulal <nilkantha.dipesh@gmail.com>
2021-09-07 11:53:59 +0545

    feat: test for serializers

--- sample/views.py
+++ sample/views.py
@@ -3,4 +3,9 @@ from django.http import HttpResponse
 # Create your views here.
 
 def say_hello(request):
+    foo(name="dipesh", age=20, full_name="dipeshdulal")
     return render(request, 'hello.html', {'name': 'Dipesh'})
+
+def foo(**kwargs):
+    for a in kwargs:
+        print(a, kwargs[a])
