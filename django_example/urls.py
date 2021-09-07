8dafdca2737ee1265456bd3512a91de55df5841a
Dipesh Dulal <nilkantha.dipesh@gmail.com>
2021-09-07 09:57:09 +0545

    feat: added new app

--- django_example/urls.py
+++ django_example/urls.py
@@ -19,5 +19,6 @@ from django.urls.conf import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("polls/", include('polls.urls'))
+    path("polls/", include('polls.urls')),
+    path("sample/", include("sample.urls")),
 ]
