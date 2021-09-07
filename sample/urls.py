2ef4586d78001da6233140b4acf6a38742442f42
Dipesh Dulal <nilkantha.dipesh@gmail.com>
2021-09-07 15:00:00 +0545

    fix: class based views

--- sample/urls.py
+++ sample/urls.py
@@ -3,9 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    path('hello/', views.say_hello),
-    path("snippets/", views.snippet_list),
-    path("snippets/<int:pk>/", views.snippet_detail),
+    path("snippets/", views.SnippetList.as_view()),
+    path("snippets/<int:pk>/", views.SnippetDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- sample/views.py
+++ sample/views.py
@@ -1,61 +1,56 @@
-from django.http.response import HttpResponse, JsonResponse
-from django.shortcuts import render
-from rest_framework.decorators import api_view
-from rest_framework.parsers import JSONParser
-from sample.models import Snippet
+from django.http.response import Http404
+from rest_framework import serializers
+from rest_framework.response import Response
+from sample import serializer
 from sample.serializer import SnippetSerializer
+from sample.models import Snippet
+from rest_framework.views import APIView
+from rest_framework import status
 
 # Create your views here.
 
-@api_view(["GET", "POST"])
-def snippet_list(request, format=None):
+class SnippetList(APIView):
     """
-    List all the code snippets, or create a new code snippet
+    List all snippets, or create new snippet
     """
 
-    if request.method == "GET":
+    def get(self, request, format=None):
         snippets = Snippet.objects.all()
-        serializer = SnippetSerializer(snippets, many = True)
-        return JsonResponse(serializer.data, safe=False)
-    elif request.method == "POST":
-        data = JSONParser().parse(request)
-        serializer = SnippetSerializer(data=data)
+        serializer = SnippetSerializer(snippets, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = SnippetSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
-    
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(["GET", "PUT", "DELETE"])
-def snippet_detail(request, pk, format=None):
+class SnippetDetail(APIView):
     """
-    Retreive, Update or delete a snippet
+    Retreive, update or delete a snippet instance
     """
-    try:
-        snippet = Snippet.objects.get(pk=pk)
-    except Snippet.DoesNotExist:
-        return HttpResponse(status=404)
+
+    def get_object(self, pk) -> Snippet:
+        try:
+            return Snippet.objects.get(pk=pk)
+        except Snippet.DoesNotExist:
+            raise Http404
     
-    if request.method == "GET":
+    def get(self, request, pk, format=None):
+        snippet = self.get_object(pk)
         serializer = SnippetSerializer(snippet)
-        return JsonResponse(serializer.data)
-
-    elif request.method == "PUT":
-        data = JSONParser().parse(request)
-        serializer = SnippetSerializer(snippet, data=data)
+        return Response(serializer.data)
+    
+    def put(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        serializer = SnippetSerializer(snippet, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
-    
-    elif request.method == "DELETE":
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        snippet = self.get_object(pk)
         snippet.delete()
-        return HttpResponse(status=204)
-
-def say_hello(request):
-    foo(name="dipesh", age=20, full_name="dipeshdulal")
-    return render(request, 'hello.html', {'name': 'Dipesh'})
-
-def foo(**kwargs):
-    for a in kwargs:
-        print(a, kwargs[a])
+        return Response(status=status.HTTP_204_NO_CONTENT)
