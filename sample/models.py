5aacde3f883231976aa7890f123f3c29d4f74bb0
Dipesh Dulal <nilkantha.dipesh@gmail.com>
2021-09-07 14:27:27 +0545

    fix: crud operation for django models

--- sample/models.py
+++ sample/models.py
@@ -12,7 +12,7 @@ class Snippet(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100, blank=True, default='')
     code = models.TextField()
-    lineos = models.BooleanField(default=False)
+    linenos = models.BooleanField(default=False)
     language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
     style = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
 
--- sample/urls.py
+++ sample/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('hello/', views.say_hello),
+    path("snippets/", views.snippet_list),
+    path("snippets/<int:pk>/", views.snippet_detail),
 ]
